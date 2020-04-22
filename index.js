e7fb04793cde00b249450df3d6cab2805dcfe45b
DAMI KOMOLAFE <damik94@gmail.com>
2020-04-21 21:02:36 -0400

    heroku changes

--- index.js
+++ index.js
@@ -4,41 +4,41 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json())
 
 
-app.get('/lists', (req, res)=>{
+app.get('/', (req, res)=>{
     List.find({}).then(lists=>{
         res.json(lists);
     })
 
 })
 
-app.get('/lists/:song', (req, res)=>{
+app.get('/:song', (req, res)=>{
     List.find({Song: req.params.song}).then(list=>{
         res.json(list);
     })
     console.log(req.params.song)
 })
 
-app.get('/lists/name/:name', (req, res)=>{
+app.get('/name/:name', (req, res)=>{
     List.find({Name: req.params.name}).then(lists=>{
         res.json(lists);
     })
 })
 
-app.post('/lists', (req, res)=>{
+app.post('/', (req, res)=>{
     console.log(req.body);
     List.create(req.body).then(list=>{
         res.json(list);
     })
 })
 
-app.put("/lists/:id", function (req, res) {
+app.put("/:id", function (req, res) {
     List.findOneAndUpdate(
       { _id: req.params.id },req.body,{ new: true }).then(list => {
       res.json(list)
     })
   })
 
-app.delete("/lists/:id", function (req, res) {
+app.delete("/:id", function (req, res) {
     List.findOneAndRemove({ _id: req.params.id })
       .then(list => {
         res.json(list)
