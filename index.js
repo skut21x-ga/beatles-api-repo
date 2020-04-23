bae6185f5f05f400355a8b8e6560cfd45703c29d
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-04-23 11:22:33 -0400

    uploaded JWT code

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
 const app = require("express")();
 const List = require("./models/List");
 const bodyParser = require("body-parser");
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+const passport = require("./config/passport")();
+const userController = require("./controllers/users.js");
 
-app.get('/', (req, res)=>{
-    List.find({}).then(lists=>{
-        res.json(lists);
-    })
+app.use("/users", userController);
 
-})
+app.use(passport.initialize());
 
-app.get('/:song', (req, res)=>{
-    List.find({Song: req.params.song}).then(list=>{
-        res.json(list);
-    })
-    console.log(req.params.song)
-})
+app.get("/", (req, res) => {
+  List.find({}).then((lists) => {
+    res.json(lists);
+  });
+});
 
-app.get('/name/:name', (req, res)=>{
-    List.find({Name: req.params.name}).then(lists=>{
-        res.json(lists);
-    })
-})
+app.get("/:song", (req, res) => {
+  List.find({ Song: req.params.song }).then((list) => {
+    res.json(list);
+  });
+  console.log(req.params.song);
+});
 
-app.post('/', (req, res)=>{
-    console.log(req.body);
-    List.create(req.body).then(list=>{
-        res.json(list);
-    })
-})
+app.get("/name/:name", (req, res) => {
+  List.find({ Name: req.params.name }).then((lists) => {
+    res.json(lists);
+  });
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  List.create(req.body).then((list) => {
+    res.json(list);
+  });
+});
 
 app.put("/:id", function (req, res) {
-    List.findOneAndUpdate(
-      { _id: req.params.id },req.body,{ new: true }).then(list => {
-      res.json(list)
-    })
-  })
+  List.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(
+    (list) => {
+      res.json(list);
+    }
+  );
+});
 
 app.delete("/:id", function (req, res) {
-    List.findOneAndRemove({ _id: req.params.id })
-      .then(list => {
-        res.json(list)
-      })
-  })
-
+  List.findOneAndRemove({ _id: req.params.id }).then((list) => {
+    res.json(list);
+  });
+});
 
 app.set("port", process.env.PORT || 4000);
 
